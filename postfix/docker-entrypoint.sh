ef4fd7ab3ecfb533bfb3357da36fe34cba0ef761
Romain GERARD <r.gerard@criteo.com>
2020-10-23 16:21:57 +0200

    Add postfix

---
+++ postfix/docker-entrypoint.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+# postfix-wrapper.sh, version 0.1.0
+#
+# You cannot start postfix in some foreground mode and
+# it's more or less important that docker doesn't kill
+# postfix and its chilren if you stop the container.
+#
+# Use this script with supervisord and it will take
+# care about starting and stopping postfix correctly.
+#
+# supervisord config snippet for postfix-wrapper:
+#
+# [program:postfix]
+# process_name = postfix
+# command = /path/to/postfix-wrapper.sh
+# startsecs = 0
+# autorestart = false
+#
+
+trap "postfix stop" SIGINT
+trap "postfix stop" SIGTERM
+trap "postfix reload" SIGHUP
+
+# force new copy of hosts there (otherwise links could be outdated)
+#cp /etc/hosts /var/spool/postfix/etc/hosts
+
+mail_filter_url=$(curl -s https://api.github.com/repos/erebe/hmailfilter/releases/latest | grep browser_download_url | cut -d '"' -f 4)
+curl -L -o /usr/bin/hmailclassifier $mail_filter_url
+chmod +x /usr/bin/hmailclassifier
+
+# start postfix
+postfix start
+
+# lets give postfix some time to start
+sleep 3
+
+# wait until postfix is dead (triggered by trap)
+while kill -0 "`cat /var/spool/postfix/pid/master.pid | sed 's/ //g'`"; do
+  sleep 15
+done
